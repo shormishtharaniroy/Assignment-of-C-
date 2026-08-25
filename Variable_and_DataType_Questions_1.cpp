57958f98704b4a9583e49459f51630f4045209df
Shormishtha <shormishtharaniroy42@gmail.com>
2026-08-25 21:34:41 +0600

    Question 1 of Variable

---
+++ Variable_and_DataType_Questions_1.cpp
@@ -0,0 +1,11 @@
+// Question1:Inaprogram,inputthesideofasquare.Youhavetooutputtheareaofthesquare.Input:n(side)Output:n*n(area)
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cout<<"Enter the side of a square: ";
+  cin>>n;
+  cout<<"Area of the square: "<<n*n;
+}
+
