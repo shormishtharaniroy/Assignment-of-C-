7cffe67608382a4f40980dbf1e0f64e807539d8c
Shormishtha <shormishtharaniroy42@gmail.com>
2026-08-26 15:51:52 +0600

    Question 2 of Operator

---
+++ Operator_Que_2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int x=10,y=5;
+  int exp1 = (y*(x/y + x/y));//20
+  int exp2 = (y*x/y + y*x/y);//20
+  cout<<exp1<<" ";
+  cout<<exp2<<"\n";
+  return 0;
+}
