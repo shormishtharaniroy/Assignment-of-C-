b72d59629bfe73a5df4cfb4feb1fef609c00ac39
Shormishtha <shormishtharaniroy42@gmail.com>
2026-08-29 18:34:56 +0600

    Question 2 of Conditional Statements

---
+++ Conditional_Statements_Que_2.cpp
@@ -0,0 +1,19 @@
+// Write a c++ program that takes a year from the user and print whether that year is a leap year or not.
+#include<iostream>
+using namespace std;
+
+int main(){
+  int year;
+ cout<<"Enter a year: ";
+ cin>>year;
+
+ if(year%4==0 && year%100!=0){
+  cout<<year<<" is a leap year.";
+
+ }else if(year%100==0 && year%400==0){
+  cout<<year<<" is a leap year.";
+ }else{
+  cout<<year<<" is not a leap year.";
+ }
+ return 0;
+}
